08ba9a5d3753e75b56879118c3de832c0dabf31b
hlib <hlibbabii@gmail.com>
2021-09-17 13:28:40 +0200

    add query_commit and query_collection methods

--- commitexplorer/client.py
+++ commitexplorer/client.py
@@ -6,7 +6,7 @@ from requests import Response
 from requests.adapters import HTTPAdapter
 from requests_cache import CachedSession
 from urllib3 import Retry
-from typing import Optional, Dict
+from typing import Dict, List, Union
 
 retry_strategy = Retry(total=10, backoff_factor=10)
 http_adapter = HTTPAdapter(max_retries=retry_strategy)
@@ -26,21 +26,37 @@ class CommitNotFoundException(Exception):
     pass
 
 
-def _query_commit_explorer_ironspeed(sha: str) -> Response:
-    IRONSPEED_URL = "http://squirrel.inf.unibz.it:8180/ce"
-    return http_session.get(f"{IRONSPEED_URL}/{sha}")
+BACKEND = 'squirrel'
 
 
-def query_commit_explorer(sha: str) -> Optional[Dict]:
+def query_backend(url: str) -> Response:
+    if BACKEND == 'squirrel':
+        backend_url = 'http://squirrel.inf.unibz.it:8180'
+    elif BACKEND == 'ironspeed':
+        backend_url = 'http://10.10.20.160:8180'
+    else:
+        raise ValueError(f'Unknown backend: {BACKEND}')
+
+    full_url = f"{backend_url}/ce/{url}"
+    return http_session.get(full_url)
+
+
+def _query_commit_explorer(url: str) -> Union[None, Dict, List]:
     try:
-        response = _query_commit_explorer_ironspeed(sha)
+        response = query_backend(url)
     except Exception as e:
         raise CommitExplorerClientException("Error while sending request to CommitExplorer backend") from e
-    if response.status_code == 404:
-        raise CommitNotFoundException(f"Commmit {sha} not found in CommitExplorer")
-    elif response.status_code != 200:
+    if response.status_code != 200:
         raise CommitExplorerClientException(f"ClientExplorer backend returned error status code: {response.status_code}")
     else:
         return json.loads(response.text)
+
+
+def query_collection(collection_name: str) -> List[str]:
+    return _query_commit_explorer(f'query/{collection_name}')
+
+
+def query_commit(sha: str, projection: Dict) -> Dict:
+    return _query_commit_explorer(f'commit/{sha}')
 
 
