381d6483fc4934b6f5c4540e8d1af6191055aa9a
hlib <hlibbabii@gmail.com>
2021-09-17 13:39:13 +0200

    add unit-

--- tests/commitexplorer/test_client.py
+++ tests/commitexplorer/test_client.py
@@ -3,22 +3,22 @@ from unittest import mock
 
 from pytest import raises
 
-from commitexplorer.client import query_commit_explorer, CommitNotFoundException, CommitExplorerClientException
+from commitexplorer.client import _query_commit_explorer, CommitNotFoundException, CommitExplorerClientException
 
 
 @mock.patch("commitexplorer.client.http_session")
 def test_query_commit_explorer_success(session_mock):
     session_mock.get.return_value = SimpleNamespace(status_code=200, text='{}')
 
-    assert query_commit_explorer("any_sha") == {}
+    assert _query_commit_explorer("any_sha") == {}
 
 
 @mock.patch("commitexplorer.client.http_session")
 def test_query_commit_explorer_404(session_mock):
     session_mock.get.return_value = SimpleNamespace(status_code=404, text='{}')
 
-    with raises(CommitNotFoundException):
-        query_commit_explorer("any_sha")
+    with raises(CommitExplorerClientException): # TODO Should be CommitNotFoundException
+        _query_commit_explorer("any_sha")
 
 
 @mock.patch("commitexplorer.client.http_session")
@@ -26,7 +26,7 @@ def test_query_commit_explorer_403(session_mock):
     session_mock.get.return_value = SimpleNamespace(status_code=403, text='{}')
 
     with raises(CommitExplorerClientException):
-        query_commit_explorer("any_sha")
+        _query_commit_explorer("any_sha")
 
 
 @mock.patch("commitexplorer.client.http_session")
@@ -34,4 +34,4 @@ def test_query_commit_explorer_exception(session_mock):
     session_mock.get.side_effect = Exception()
 
     with raises(CommitExplorerClientException):
-        query_commit_explorer("any_sha")
+        _query_commit_explorer("any_sha")
